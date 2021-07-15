b215873abbfd78925f48da2ec20213079f72e24d
minhduc <minhduc@DESKTOP-VU5DS1R>
2021-07-16 00:29:20 +0700

    database_design

---
+++ Data/EF_DbContext/DemoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.EF_DbContext
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<user> Users { get; set; }
+        DbSet<file> Files { get; set; }
+    }
+}
---
+++ Data/Entities/user.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string username {get;set;}
+        public string password { get; set; }
+        public string email { get; set; }
+        public string firt_name { get; set; }
+        public string last_name { get; set; }    
+        public string phone { get; set; }
+    }
+}
