6de92bea952c457d7cced5f38f1c9ae87ec4a5f4
minhduc <minhduc@DESKTOP-VU5DS1R>
2021-07-16 17:20:53 +0700

    add-migration

---
+++ Data/EF_DbContext/DemoDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Data.EF_DbContext
+{
+    public class DemoDbContextFactory : IDesignTimeDbContextFactory<DemoDbContext>
+    {
+        public DemoDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("THL_ONE_DEMO_DB");
+            var optionsBuilder = new DbContextOptionsBuilder<DemoDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new DemoDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Data/EF_DbContext/SeedData.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.EF_DbContext
+{
+    public static class SeedData
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<user>().HasData(new user() { id = 1, username = "admin", password = "p@ssword", email = "admin@gmail.com", phone = "0123456789",firt_name = "Đỗ", last_name = "Minh Đức" });
+        }
+    }
+}
